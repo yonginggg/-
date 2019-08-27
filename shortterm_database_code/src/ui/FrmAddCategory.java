77a2ba06581140d13080f8aee832de15a97ec8cb
yonginggg <843852754@qq.com>
2019-08-27 14:19:20 +0800

    Create FrmAddCategory.java
    添加食材类别

---
+++ shortterm_database_code/src/ui/FrmAddCategory.java
@@ -0,0 +1,78 @@
+package ui;
+
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JDialog;
+
+import kitchen.control.IngredientsManager;
+import kitchen.util.*;
+
+public class FrmAddCategory extends JDialog implements ActionListener {
+
+	private JPanel toolBar = new JPanel();
+	private JPanel workPane = new JPanel();
+	private JButton btnOk = new JButton("确定");
+	private JButton btnCancel = new JButton("取消");
+	private JLabel labelName = new JLabel("名称：");
+	private JLabel labelDescription = new JLabel("描述：");
+
+	private JTextField edtName = new JTextField(20);
+	private JTextField edtDescription = new JTextField(20);
+
+	public FrmAddCategory(JFrame f, String s, boolean b) {
+		super(f, s, b);
+		toolBar.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		toolBar.add(btnOk);
+		toolBar.add(btnCancel);
+		this.getContentPane().add(toolBar, BorderLayout.SOUTH);
+		workPane.add(labelName);
+		workPane.add(edtName);
+		workPane.add(labelDescription);
+		workPane.add(edtDescription);
+		this.getContentPane().add(workPane, BorderLayout.CENTER);
+		this.setSize(320, 180);
+		// 屏幕居中显示
+		double width = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		this.setLocation((int) (width - this.getWidth()) / 2, (int) (height - this.getHeight()) / 2);
+
+		this.validate();
+		this.btnOk.addActionListener(this);
+		this.btnCancel.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+		if (e.getSource() == this.btnCancel) {
+			this.setVisible(false);
+			return;
+		} else if (e.getSource() == this.btnOk) {
+			String name = this.edtName.getText();
+			String description = this.edtDescription.getText();
+			IngredientsManager ingredientsManager = new IngredientsManager();
+			try {
+				ingredientsManager.addIngredientsCategory(name, description);
+				this.setVisible(false);
+			} catch (BaseException e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "错误", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+		}
+
+	}
+
+}
